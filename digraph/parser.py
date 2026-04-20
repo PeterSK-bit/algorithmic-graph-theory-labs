773b3a12b9882a62806b9a66fc28f774bfde5a7a
Peter Magdík <petermagdik@gmail.com>
2026-04-20 12:30:47 +0200

    fix: fixed wrong parsing of digraph vertecies

--- digraph/parser.py
+++ digraph/parser.py
@@ -2,6 +2,7 @@ from io import TextIOWrapper
 
 from digraph.digraph import Digraph
 from digraph.oriented_edge import OrientedEdge
+from basics.vertex import Vertex
 
 class Parser:
     def __init__(self, file: TextIOWrapper):
@@ -18,5 +19,15 @@ class Parser:
         return biggest_number
 
     def parse_to_graph(self) -> Digraph:
-        return Digraph.from_edges([OrientedEdge(*map(int, line.split())) for line in self.file.readlines()])
-        
+        edges = []
+        for line in self.file.readlines():
+            u, v, weight = map(int, line.split())
+            edges.append(OrientedEdge(Vertex(u), Vertex(v), weight))
+        return Digraph.from_edges(edges)
+    
+    def parse_to_activities(self) -> dict[int, int]:
+        activities = {}
+        for line in self.file.readlines():
+            u, v, weight = map(int, line.split())
+            activities[u] = weight
+        return activities
