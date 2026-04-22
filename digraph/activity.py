77657354521ab26c5d5013ff6c2ade2e8181182c
Peter Magdík <petermagdik@gmail.com>
2026-04-23 00:06:53 +0200

    fix: add 'self.' prefix to instance variables in Activity class constructor

--- digraph/activity.py
+++ digraph/activity.py
@@ -4,9 +4,9 @@ class Activity:
         self.duration = duration
         self.successors = successors
 
-        es: int = 0  # Earliest Start
-        ef: int = 0  # Earliest Finish
-        ls: int = 0  # Latest Start
-        lf: int = 0  # Latest Finish
-        slack: int = 0  # Time Reserve
-        is_critical: bool = False
+        self.es: int = 0
+        self.ef: int = 0
+        self.ls: int = 0
+        self.lf: int = 0
+        self.slack: int = 0
+        self.is_critical: bool = False
