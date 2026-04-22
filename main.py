10f775efd5eaf87fdc555c97bbacb75977e6d5cc
Peter Magdík <petermagdik@gmail.com>
2026-04-22 16:19:25 +0200

    refactor: migrate graph storage from TXT to JSON and simplify vertex representation

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from file_loader import load_file
-from graph.parser import Parser
+from digraph.parser import Parser
 
-from traveling_salesman import Traveling_salesman
+from time_planning import TimePlanning
 
 def main():
     parser = Parser(load_file("data/graph2.txt"))
     g = parser.parse_to_graph()
-    tsm = Traveling_salesman(g)
-    tour = tsm.solve_greedy(1)
-    print("Tour:", tour)
-    
+    activities = parser.parse_to_activities() #TODO implement this method
+    tp = TimePlanning(g, activities)
+
 if __name__ == "__main__":
     main()
