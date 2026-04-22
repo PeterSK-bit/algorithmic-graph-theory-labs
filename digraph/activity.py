7e34f38455443236c854143dd5742d4fc2547ba8
Peter Magdík <petermagdik@gmail.com>
2026-04-22 19:57:29 +0200

    feat: initialize additional attributes in Activity class for scheduling calculations

--- digraph/activity.py
+++ digraph/activity.py
@@ -3,3 +3,10 @@ class Activity:
         self.id = id
         self.duration = duration
         self.depends_on = depends_on
+
+        es: int = 0  # Earliest Start
+        ef: int = 0  # Earliest Finish
+        ls: int = 0  # Latest Start
+        lf: int = 0  # Latest Finish
+        slack: int = 0  # Time Reserve
+        is_critical: bool = False
